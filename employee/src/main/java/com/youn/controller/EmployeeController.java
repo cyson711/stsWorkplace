8b19422fea476ea76651c9ed40afa59ac85f6d56
Chaeyoun Son <cy.son97@gmail.com>
2022-03-24 10:46:12 +0900

    1차저장
    get api 까지

--- employee/src/main/java/com/youn/controller/EmployeeController.java
+++ employee/src/main/java/com/youn/controller/EmployeeController.java
@@ -6,14 +6,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.youn.dao.EmployeeDao;
 import com.youn.vo.Employee;
 
 @RestController
-@RequestMapping("/")
+
 public class EmployeeController {
+	
 	@Autowired
 	EmployeeDao empDao;
 	
@@ -23,7 +25,9 @@ public class EmployeeController {
 		return "index.html";
 	}*/
 	
-	@GetMapping
+	
+	
+	@RequestMapping(value = "/search", method = RequestMethod.GET)
 	public List<Employee> list() {
 		return empDao.employeeList();
 	}
